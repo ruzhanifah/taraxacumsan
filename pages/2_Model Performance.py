b4952d0dce1a353669192a79cfd80888c6102d62
ruzhanifah <ruzhanifah@gmail.com>
2025-05-22 09:22:08 +0700

    Update 2_Model Performance.py

--- pages/2_Model Performance.py
+++ pages/2_Model Performance.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
+
+st.set_page_config(page_title="Model Performance")
+st.title("Model Performance")
+st.header("Model Performance")
 
