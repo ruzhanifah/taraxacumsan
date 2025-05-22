ca055f98cc74331fc830d9ee2e7d500d9425cbe0
ruzhanifah <ruzhanifah@gmail.com>
2025-05-22 09:22:49 +0700

    Update 3_Prediction.py

--- pages/3_Prediction.py
+++ pages/3_Prediction.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
 
+st.set_page_config(page_title="Prediction")
+st.title("Prediction")
+st.header("Prediction")
